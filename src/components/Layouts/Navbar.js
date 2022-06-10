5ebd03809820f1b0760d978e8e1a5606ecce2838
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2022-06-10 22:12:20 +0600

    navbar updated

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -10,6 +10,7 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { Logo, SpecialNavBox } from '../SVGs'
 import MediaNav from '../MediaNav'
 import NavLink from '../NavLink'
+import Button from '../Button'
 // import { getCurrentBreakPoint, useWidth } from "../helpers/screensRes";
 
 const Navbar = () => {
@@ -33,7 +34,7 @@ const Navbar = () => {
     const SideBar = () => {
         return (
             <div ref={sideBarRef}>
-                <ul className="absolute transition-all duration-500 flex flex-col top-16 w-3/5 md:w-1/2 h-screen bg-btn-color font-body font-bold z-50 left-0 pl-4">
+                <ul className="absolute transition-all duration-500 flex flex-col top-16 w-2/5 sm:w-1/2 xs:w-145 h-screen bg-btn-color font-body font-bold z-50 left-0 pl-4">
                     <li className="navlink text-white text-2xl pt-16 pb-8">
                         {/* <Link href="/">
               <a className="hover:opacity-70 transition-all duration-500">
@@ -44,7 +45,6 @@ const Navbar = () => {
                             href="/"
                             classname="hover:opacity-70 transition-all
                             duration-500">
-                            {' '}
                             Home
                         </NavLink>
                     </li>
@@ -59,7 +59,6 @@ const Navbar = () => {
                             href="/courses"
                             classname="hover:opacity-70 transition-all
                             duration-500">
-                            {' '}
                             Courses
                         </NavLink>
                     </li>
@@ -74,7 +73,6 @@ const Navbar = () => {
                             href="/freeclasses"
                             classname="hover:opacity-70 transition-all
                             duration-500">
-                            {' '}
                             Free Classes
                         </NavLink>
                     </li>
@@ -95,7 +93,6 @@ const Navbar = () => {
                             href="/portfolio"
                             classname="hover:opacity-70 transition-all
                             duration-500">
-                            {' '}
                             Portfolio
                         </NavLink>
                     </li>
@@ -110,9 +107,16 @@ const Navbar = () => {
                             href="/shop"
                             classname="hover:opacity-70 transition-all
                             duration-500">
-                            {' '}
                             Shop
                         </NavLink>
+                    </li>
+
+                    <li className="pt-16">
+                        <Link href="/join">
+                            <Button className="bg-white text-btn-color rounded-lg hover:opacity-70 transition-all duration-500 w-40">
+                                Join
+                            </Button>
+                        </Link>
                     </li>
 
                     {/* {!auth && !isLoggedIn() ? (
@@ -174,13 +178,19 @@ const Navbar = () => {
     return (
         <>
             <div>
-                <nav className="flex justify-between min-w-full h-40 md:h-24 sm:h-28 flex-col bg-main-color text-body shadow-md px-52 2xl:px-28 xl:px-28 lg:px-28 md:px-0 sm:px-0">
-                    <div className="flex justify-between sm:justify-center large:justify-center">
+                <nav className="flex justify-between min-w-full h-40 md:h-24 sm:h-28 flex-col bg-main-color text-body shadow-md px-52 2xl:px-28 xl:px-28 lg:px-12 md:px-0 sm:px-0">
+                    <div className="flex justify-between md:justify-center">
                         {/* Logo */}
                         <Link href="/">
                             <a>
-                                <Logo className="relative top-4 large:-top-4 md:left-2/4 sm:-top-4 sm:left-2/4 transform -translate-x-2/4 p-4 sm:p-7 sm:mb-4 sm:ml-4 sm:mt-4 left-32 sm:z-40" />
+                                <Logo className="relative top-4 md:-top-4 md:left-2/4 sm:-top-4 sm:left-2/4 transform -translate-x-2/4 p-4 sm:p-7 left-32 lg:left-28 sm:z-40" />
                             </a>
+                        </Link>
+
+                        <Link href="/join">
+                            <Button className="w-36 bg-btn-color rounded-lg outline-none hover:scale-105 transform transition-all duration-500 md:hidden sm:hidden self-end absolute top-12 right-52 2xl:right-28 xl:right-28 lg:right-12">
+                                Join
+                            </Button>
                         </Link>
 
                         {/* {!auth && !isLoggedIn() ? (
@@ -198,7 +208,7 @@ const Navbar = () => {
                       )} */}
                     </div>
 
-                    <ul className="md:hidden font-body font-bold sm:hidden list-none flex justify-between items-center w-1/2 2xl:w-133 lg:w-146 self-end pb-2">
+                    <ul className="md:hidden font-body font-bold sm:hidden list-none flex justify-between items-center w-3/5 lg:w-146 self-end">
                         <li className="navlink hover:text-btn-color xl:text-base lg:text-base">
                             {/* <Link href="/">
                               <a>Home</a>
