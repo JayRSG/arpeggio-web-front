54f7bad74dd2ca82d54297e19311ae49b03a95aa
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2022-06-10 22:11:34 +0600

    button updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ type = 'submit', className, ...props }) => (
     <button
         type={type}
-        className={`${className} btn bg-btn-color w-56 h-14 shadow-btn text-2xl mb-5 font-title font-bold`}
+        className={`${className} btn w-56 h-14 shadow-btn text-2xl mb-5 font-title font-bold`}
         {...props}
     />
 )
