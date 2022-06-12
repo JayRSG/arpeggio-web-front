edb0dba5c84fdf489d636a829a5cbe9e629bfbb2
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2022-06-12 21:02:02 +0600

    home updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,41 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useAuth } from '@/hooks/auth'
+// import { useAuth } from '@/hooks/auth'
 import Navbar from '@/components/Layouts/Navbar'
 import Footer from '@/components/Layouts/Footer'
+import GuestLayout from '@/components/Layouts/GuestLayout'
+import Home from '@/components/Home'
 
 export default function home() {
     // const { user } = useAuth({ middleware: 'guest' })
 
     return (
         <>
-            <Head>
-                <title>Arpeggio</title>
-                <meta property="og:url" content="www.arpeggiobd.com" />
-                <meta property="og:type" content="website" />
-                <meta property="fb:app_id" content="867124930576205" />
-                <meta property="og:title" content={'Arpeggio Music School'} />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:image" content={'image/home-2.jpg'} />
-                <meta
-                    property="og:description"
-                    content="The courses we offer are - Guitar, Piano, Violin, Bass, Drums, Ukulele, Flute and Painting. Besides regular courses we have many short-term courses as well. We also offer private classes and online music courses for students staying abroad or far away from our location."
-                />
-                <meta property="og:image" content={'image/home-2.jpg'} />
-            </Head>
+            <GuestLayout>
+                <Head>
+                    <title>Arpeggio</title>
+                    <meta property="og:url" content="www.arpeggiobd.com" />
+                    <meta property="og:type" content="website" />
+                    <meta property="fb:app_id" content="867124930576205" />
+                    <meta
+                        property="og:title"
+                        content={'Arpeggio Music School'}
+                    />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:image" content={'image/home-2.jpg'} />
+                    <meta
+                        property="og:description"
+                        content="The courses we offer are - Guitar, Piano, Violin, Bass, Drums, Ukulele, Flute and Painting. Besides regular courses we have many short-term courses as well. We also offer private classes and online music courses for students staying abroad or far away from our location."
+                    />
+                    <meta property="og:image" content={'image/home-2.jpg'} />
+                </Head>
 
-            <Navbar />
-            {/* <Home /> */}
+                <Navbar />
 
-            <Footer />
+                <Home />
+
+                <Footer />
+            </GuestLayout>
         </>
     )
 }
