041adecdd1cda8bff1d83c83a2828900e8623d8c
Jay R SG <jaysen110@gmail.com>
2022-06-14 01:39:02 +0600

    shop, freeClass

---
+++ src/pages/freeClasses.js
@@ -0,0 +1,19 @@
+import Footer from "../components/Layouts/Footer";
+import FreeClasses from "../components/FreeClasses";
+import Navbar from "../components/Layouts/Navbar";
+
+
+const freeClasses = () => {
+  return (
+    <>
+      <title>Free Classes</title>
+      <Navbar />
+
+      <FreeClasses />
+
+      <Footer />
+    </>
+  );
+};
+
+export default freeClasses;
---
+++ src/pages/shop.js
@@ -0,0 +1,17 @@
+import ComingSoon from '../components/ComingSoon'
+import Footer from '../components/Layouts/Footer'
+import Navbar from '../components/Layouts/Navbar'
+
+const Shop = () => {
+    return (
+        <>
+            <Navbar />
+
+            <ComingSoon />
+
+            <Footer />
+        </>
+    )
+}
+
+export default Shop
