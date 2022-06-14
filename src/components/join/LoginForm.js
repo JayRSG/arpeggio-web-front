0a95982c19071376c1380a9c485933f4b08433b4
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2022-06-14 22:51:24 +0600

    loginform updated

--- src/components/join/LoginForm.js
+++ src/components/join/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = () => {
     }
     return (
         <>
-            <div className='flex flex-col justify-center mt-3 flex-wrap'>
+            <div className="flex flex-col justify-center mt-3 flex-wrap">
                 {/* Session Status */}
                 <AuthSessionStatus className="mb-4" status={status} />
 
