a3ebf6f34358d2798ba277b3711b54cc88d37325
Jay R SG <jaysen110@gmail.com>
2022-10-11 19:43:26 +0600

    Google Analytics Integration pageviews and events

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,52 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import * as gtag from "@/lib/gtag"
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Script from "next/script";
 
 config.autoAddCss = false
 
 const App = ({ Component, pageProps }) => {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events])
+
     return (
         <>
+
             <Head>
                 <link rel="icon" href="/fav-icon.png" />
             </Head>
+
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+
+                    function gtag(){dataLayer.push(arguments);}
+                    
+                    gtag('js', new Date());
+                    
+                    gtag('config', '${gtag.GA_TRACKING_ID}',
+                    {
+                        page_path: window.location.pathname,
+                    });`,
+                }}
+            />
 
             {<Component {...pageProps} />}
         </>
